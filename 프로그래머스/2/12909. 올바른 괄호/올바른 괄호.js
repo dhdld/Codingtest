ac75cf66cb2e6526be90ac676a1a4e854ed04b93
dhdld <54532519+dhdld@users.noreply.github.com>
2024-02-27 16:32:23 +0900

    [level 2] Title: 올바른 괄호, Time: 2.50 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,9 @@
+function solution(s){
+    if( s[0] == ')') return false;
+    let cnt =0;
+    for(let i=0; i<s.length; i++){
+        s[i]=="(" ? cnt+=1 : cnt-=1;
+        if(cnt<0) return false;
+    }
+    return cnt==0 ? true : false;
+}
