6ceeb68061a2bd94ebf26751103b9a0b1bdac168
dhdld <54532519+dhdld@users.noreply.github.com>
2024-02-28 13:36:10 +0900

    [level 2] Title: 올바른 괄호, Time: 3.09 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,5 +1,5 @@
 function solution(s){
-    if( s[0] == ')') return false;
+    if(s[0] == ')') return false;
     let cnt =0;
     for(let i=0; i<s.length; i++){
         s[i]=="(" ? cnt+=1 : cnt-=1;
