531e3a6801dcc1edcc757d117c8b7ee6a5c9592c
dhdld <54532519+dhdld@users.noreply.github.com>
2024-03-01 17:18:18 +0900

    [level 2] Title: 숫자의 표현, Time: 2.19 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -2,12 +2,13 @@ function solution(n) {
     let cnt=1;
     for(let i=1; i<= n/2; i++){
         let sum=0;
-        for(let j=i; sum<n; j++){
+        for(let j=i; j<=n/2+1; j++){
             sum+=j;
             if(sum==n) {
                 cnt++;
-                //break;
+                break;
             }
+            if(sum+j > n) break;
         }
     }
     return cnt;
