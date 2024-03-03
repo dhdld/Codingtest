edbbbd88196432f6df13ee561e5ec83565e0115d
dhdld <54532519+dhdld@users.noreply.github.com>
2024-03-03 15:52:25 +0900

    [level 2] Title: 짝지어 제거하기, Time: 44.61 ms, Memory: 46.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const stack=[];
+    for(let i=0; i<s.length; i++){
+        if(s[i]==stack[stack.length-1]) stack.pop();
+        else stack.push(s[i]);
+    }
+    return stack[0] ? 0 : 1;
+}
