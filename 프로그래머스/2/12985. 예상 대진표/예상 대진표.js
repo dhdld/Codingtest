f71aebdc34a54f47c4c5e1e228d90ad52296f21c
dhdld <54532519+dhdld@users.noreply.github.com>
2024-03-10 14:12:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,18 @@
+function solution(n,a,b)
+{
+    let answer = 0;
+    while(n>=2){
+        answer++;
+        if(a%2==1 && b-a==1)
+           return answer;
+        else if(b%2==1 && a-b==1)
+            return answer;
+        
+        n=n/2;
+        if(a%2==0) a=a/2;
+        else a=(a+1)/2;
+        if(b%2==0) b=b/2;
+        else b=(b+1)/2;
+    }
+    //return answer;
+}
