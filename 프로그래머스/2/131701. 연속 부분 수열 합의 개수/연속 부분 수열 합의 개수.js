d150ba4501aafe7136a8a6d94f62a434d6559b5a
dhdld <54532519+dhdld@users.noreply.github.com>
2024-03-12 23:28:21 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1221.94 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,17 +2,17 @@ function solution(elements) {
     const set = new Set();
     const getSum = (arr) =>{
         let sum=0;
-        for(let i=0;i<arr.length;i++)
+        for(let i=0; i<arr.length; i++)
             sum+=arr[i];
         return sum;
     }
     let len = elements.length;
     for(let i=1; i<=len; i++){
-        for(let j=0; j<len;j++){
+        for(let j=0; j<len; j++){
             if(j+i>len)
-                set.add(getSum(elements.slice(j,len))+getSum(elements.slice(0,j+i-len)));
-            else
-                set.add(getSum(elements.slice(j,j+i)));
+                set.add(getSum(elements.slice(j,len))+getSum(elements.slice(0,j+i-len)))
+            else 
+                set.add(getSum(elements.slice(j,j+i)))
         }
     }
     return set.size;
