06084a00bca733cb2fdefffbc8dd3c352e10b84e
김소연 <54532519+dhdld@users.noreply.github.com>
2024-05-11 00:36:19 +0900

    [Bronze II] Title: 단어의 개수, Time: 212 ms, Memory: 18228 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,7 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+let cnt = 0;
+for (let i = 0; i < input.length; i++) {
+    if (input[i] !== "")
+        cnt++;
+}
+console.log(cnt)
