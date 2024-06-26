28ea8a97c1e0b5e4b191439c6bb3f78baca1b991
김소연 <54532519+dhdld@users.noreply.github.com>
2024-06-26 15:17:14 +0900

    [Silver IV] Title: 수들의 합 2, Time: 228 ms, Memory: 10036 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -1,8 +1,5 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
-let ex = input[0].split(' ').map(Number);
-let n = ex[0]
-let max = ex[1]
+let [n, max] = input[0].split(' ').map(Number);
 let arr = input[1].split(' ').map(Number);
 let cnt = 0;
 for (let i = 0; i < n; i++) {
