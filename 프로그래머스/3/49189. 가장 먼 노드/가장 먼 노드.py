433d3479eb94557d997dbb409994aa3f91ef43ad
김소연 <54532519+dhdld@users.noreply.github.com>
2025-04-24 14:33:27 +0900

    [level 3] Title: 가장 먼 노드, Time: 149.63 ms, Memory: 19.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def solution(n, edge):
+    edge = sorted(edge)
+    d=[0]*(n+1)
+    queue=deque()
+    graph=[[] for _ in range(n+1)]
+    answer=0
+    
+    for i in edge:
+        graph[i[0]].append(i[1])
+        graph[i[1]].append(i[0])
+
+    queue.append(1)
+    d[1]=1
+    while queue:
+        cur = queue.popleft()
+        for node in graph[cur]:
+            if d[node] == 0:
+                queue.append(node)
+                d[node] = d[cur]+1
+                
+    max_d = max(d)
+    for j in d:
+        if j == max_d:
+            answer+=1
+    return answer
+                
+    
