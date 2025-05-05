294bf7abef37af70aac83f1f0a8a729486d5527c
김소연 <54532519+dhdld@users.noreply.github.com>
2025-05-05 16:22:31 +0900

    [Silver IV] Title: 스택, Time: 104 ms, Memory: 12040 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -1,5 +1,6 @@
 const [n, ...arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
 const stack=[]
+const res = []
 for(let i=0; i<n; i++){
     if (arr[i].includes('push')){
         let num = arr[i].split(' ')
@@ -7,25 +8,27 @@ for(let i=0; i<n; i++){
     }
     else if (arr[i].includes('pop')){
         if (stack.length == 0)
-            console.log(-1)
+            res.push(-1)
         else {
-           console.log(stack[stack.length-1])
+           res.push(stack[stack.length-1])
             stack.pop()
         }
     }
     else if (arr[i].includes('size')) {
-        console.log(stack.length)
+        res.push(stack.length)
     }
     else if (arr[i].includes('empty')){
         if (stack.length == 0)
-            console.log(1)
-        else console.log(0)
+            res.push(1)
+        else res.push(0)
     }
     else if (arr[i].includes('top')){
         if (stack.length == 0)
-            console.log(-1)
+            res.push(-1)
         else {
-           console.log(stack[stack.length-1])
+           res.push(stack[stack.length-1])
         }
     }   
 }
+
+console.log(res.join('\n'))
