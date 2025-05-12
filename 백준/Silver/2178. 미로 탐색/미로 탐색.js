45a5b24b4a134d7d837bbb163846be8b15bf08f8
김소연 <54532519+dhdld@users.noreply.github.com>
2025-05-12 10:14:18 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 11868 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,29 +1,29 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const [n,m] = input.shift().split(' ').map(Number)
+const map = input.map((v)=>v.split('').map(Number))
 
-const [N, M] = input.shift().split(' ').map(Number);
-const maze = input.map((v) => v.split('').map(Number));
-const [goalY, goalX] = [N - 1, M - 1]; // 도착 위치
-const ds = [[-1, 0], [1, 0], [0, 1], [0, -1]];
+const [goalY, goalX] = [n-1,m-1] // 도착 위치
+const ds = [[-1,0], [1,0], [0,-1], [0,1]] // 상하좌우 칸들
 
 const bfs = () => {
-  const queue = [[0, 0, 1]]; // 좌표 및 움직인 칸 수
-
-  while (queue.length) {
-    const [curY, curX, move] = queue.shift();
-
-    if (goalY === curY && goalX === curX) return move;
-
-    for (let i = 0; i < 4; i++) {
-      const ny = curY + ds[i][1];
-      const nx = curX + ds[i][0];
-
-      // 다음 위치가 미로 밖으로 벗어나지 않고 길이 있는 곳(1)이면
-      if (ny >= 0 && ny < N && nx >= 0 && nx < M && maze[ny][nx]) {
-        maze[ny][nx] = 0; // 방문 처리
-        queue.push([ny, nx, move + 1]); // 다음 위치넣고 한 칸 이동
-      }
+    const queue = [[0,0,1]] // 좌표, 움직인 칸 수
+    while(queue.length){
+        const [y, x, move] = queue.shift()
+        // 도착
+        if(goalX == x && goalY == y) return move;
+        
+        // 인접 위치 탐색. 다음 이동할 칸
+        for(let i=0; i<4; i++){
+            const ny = y+ds[i][0]
+            const nx = x + ds[i][1]
+            // 다음 위치가 미로 밖으로 벗어나지 않고 1이면
+            if(nx>=0 && nx < m && ny >=0 && ny < n && map[ny][nx]){
+                map[ny][nx]=0 // 방문처리
+                queue.push([ny,nx,move+1])
+            }
+        }
     }
-  }
-};
+}
 
-console.log(bfs());
+console.log(bfs())
+
