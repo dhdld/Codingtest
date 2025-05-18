d74839f6de9ea5d0a0b7fa58af6b2705b86abbf8
김소연 <54532519+dhdld@users.noreply.github.com>
2025-05-19 00:31:18 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,9 +1,6 @@
 function solution(date1, date2) {
-    let answer=0;
-    const [y1,m1,d1] = date1
-    const [y2,m2,d2] = date2
-    if(y1 !== y2) return y1<y2 ? 1:0
-    if(m1 !== m2) return m1<m2 ? 1:0
-    if(d1 !== d2) return d1<d2 ? 1:0
-    return answer;
+    for(let i=0; i<3; i++){
+        if(date1[i] !== date2[i]) return date1[i]<date2[i] ? 1 : 0
+    }
+    return 0;
 }
