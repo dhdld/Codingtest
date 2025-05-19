74332923ec1f4adfcbd7327552a853b99cf0a955
김소연 <54532519+dhdld@users.noreply.github.com>
2025-05-20 00:00:54 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,4 +1,3 @@
 function solution(num_list) {
-    if(num_list.length>=11) return num_list.reduce((acc,cur)=>acc+cur,0)
-    return num_list.reduce((acc, cur)=> acc*cur, 1)
+    return num_list.length>=11 ? num_list.reduce((acc,cur)=>acc+cur,0) : num_list.reduce((acc, cur)=> acc*cur, 1)
 }
